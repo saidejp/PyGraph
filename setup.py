55150a2740116a1860c9f32873d06d33f6fb32e6
Javier Villanueva-Valle <sivlemx@gmail.com>
2018-08-09 11:35:28 -0500

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PyGraph',
+      version='0.1',
+      description='Python analyze the Periodicity and Cicardians Rhythms',
+      author='Javier Villanueva-Valle',
+      author_email='javier830409@gmail.com',
+      url='',
+      packages=['pandas', 'matplotlib', 'numpy'],
+     )
